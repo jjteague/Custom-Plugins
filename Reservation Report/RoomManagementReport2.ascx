457ba9e17dbf8f52394ed73382a46c811429e6f0
Ethan Widen <EDWiden96@gmail.com>
2019-12-31 11:30:48 -0600

    Add Room Reservation Report

---
+++ Reservation Report/RoomManagementReport2.ascx
@@ -0,0 +1,127 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="RoomManagementReport2.ascx.cs" Inherits="RoomManagementReport2" %>
+<style>
+    .table>thead>tr>th, .table>tbody>tr>th, .table>tfoot>tr>th, .table>thead>tr>td, .table>tbody>tr>td, .table>tfoot>tr>td {
+        border: 0;
+    }
+    .location-header {
+        text-align: center;
+        font-size: 2em;
+    }
+    .info-header {
+        text-align:center;
+        font-size: 2em;
+        background-color: #A3CAF3 !important;
+    }
+    .day-header {
+        font-size: 1.2em;
+        font-weight: bold;
+        background-color: #FFEEA2 !important;
+    }
+    .font-italic {
+        font-style: italic;
+    }
+
+    @media print
+    {
+		@page { margin: 0; }
+		body { margin: 0; }
+        .table > tbody > tr > td {
+            font-size: 1em;
+        }
+        .table > tbody > tr {
+            font-size: 1.1em;
+        }
+        .info-header {
+            text-align:center;
+            font-size: 3em;
+            background-color: #A3CAF3 !important;
+        }
+        .day-header {
+            font-size: 2em;
+            font-weight: bold;
+            background-color: #FFEEA2 !important;
+        }
+
+        .navbar .navbar-fixed-top .rock-top-header {
+            display: none !important;
+        }
+        .navbar-default .navbar-static-side {
+            display: none !important;
+        }
+         .navbar-default .navbar-static-side > li {
+            display: none !important;
+        }
+         .nav .nav-stacked .navbar-side {
+             display: none !important;  
+         }
+         .fa fa-book {
+             display: none !important;
+         }
+        .panel-body {
+            border: 0;
+        }
+        .page-title {
+            display: none !important;
+        }
+        #content-wrapper {
+            margin: 0 !important;
+        }
+        .panel .panel-body {
+            border: 0;
+        }
+        .panel {
+            box-shadow: none;
+            border: 0;
+        }
+        .main-footer {
+            display: none !important;
+        }
+		body
+		{
+		  padding: -10cm 0mm 0mm 0mm;
+		}
+		.book-date {
+    page-break-after: always;
+}
+
+    }
+
+</style>
+<div class="row">
+
+    <div class="col-xs-12 hidden-print">
+
+        <div class="panel panel-block">
+            <div class="panel-body">
+                <div class="row">
+                        <div class="col-xs-12" style="margin:10px 0px 0px 0px">
+                            <asp:Label Text="Date Range" runat="server" />
+                            </div>
+                    <div class="col-xs-12" style="margin:0px 0px 10px 0px">
+                            <Rock:DateRangePicker runat="server" ID="dateRangePicker" />
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-12">
+        <div class="panel panel-block">
+            <div class="panel-heading hidden-print">
+                <h1 class="panel-title">All Building Combined Report</h1>
+				
+					<a href="#" class="btn btn-primary hidden-print pull-right" onClick="window.print();"><i class="fa fa-print"></i> Print Statement</a>
+                    <span class="pull-right" style="padding-right:15px;"><Rock:BootstrapButton ID="btn_submit" runat="server" OnClick="btn_submit_Click" Text="Refresh" CssClass="btn btn-primary pull-right" ToolTip="Click here to adjust the date range" /></span>
+            </div>
+            <div class="panel-body">
+        
+                    
+                        <asp:Panel runat="server" ID="mainDiv" />
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
---
+++ Reservation Report/RoomManagementReport2.ascx.cs
@@ -0,0 +1,145 @@
+﻿/// Author: Ethan Widen
+/// Email: ethan@dtschurch.com
+/// Created Date: 5/15/2019
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.UI.WebControls;
+using com.centralaz.RoomManagement.Model;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+[DisplayName("Room Management Report All Building")]
+[Category("com_DTS > Room Management Reports")]
+[Description("Room Management Report Will Print ALL Building List Combined")]
+public partial class RoomManagementReport2 : RockBlock
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            
+        }
+
+    }
+
+
+
+    protected void btn_submit_Click( object sender, EventArgs e )
+    {
+        BindGrid();
+    }
+
+    private void BindGrid()
+    {
+        var rockContext = new RockContext();
+        var reservationService = new ReservationService( rockContext );
+        var qry = reservationService.Queryable();
+        var locationsService = new LocationService( rockContext ).Queryable().AsNoTracking();
+        DateTime lowerDate = dateRangePicker.LowerValue ?? DateTime.Now;
+        DateTime upperDate = dateRangePicker.UpperValue ?? DateTime.Now;
+        DateTime now = DateTime.Now;
+        var allLocations = locationsService.ToList().Where( l => l.IsNamedLocation == true );
+
+        var reservationSummaryList = reservationService.GetReservationSummaries( qry, lowerDate, upperDate, false );
+
+        Label infoLabel = new Label();
+        Panel infoPanel = new Panel();
+        infoLabel.Text = "WHAT'S HAPPENING AT SB THIS WEEK";
+        infoPanel.CssClass = "col-xs-12 info-header";
+        infoPanel.Controls.Add( infoLabel );
+        mainDiv.Controls.Add( infoPanel );
+
+        foreach (DateTime day in EachDay( lowerDate, upperDate ))
+        {
+
+            int numOfRes = reservationSummaryList.Where( r => r.ReservationStartDateTime.Date == day.Date).Count();
+            if (numOfRes > 0)
+            {
+                Label dayLabel = new Label();
+                Panel dayPanel = new Panel();
+                dayLabel.Text = String.Format( "{0}", day.ToShortDateString() );
+                dayPanel.Controls.Add( dayLabel );
+                dayPanel.CssClass = "col-xs-2 day-header";
+                mainDiv.Controls.Add( dayPanel );
+
+
+                Panel resDayPanel = new Panel();
+                Table table = new Table();
+                table.CssClass = "table table-sm";
+                TableHeaderRow tableHeaderRow = new TableHeaderRow();
+                TableHeaderCell thName = new TableHeaderCell();
+                TableHeaderCell thEventStartEnd = new TableHeaderCell();
+                TableHeaderCell thEventLocations = new TableHeaderCell();
+                thName.Text = "Name";
+				thName.CssClass = "col-xs-4";
+                thEventStartEnd.Text = "Event Time";
+				thEventStartEnd.CssClass = "col-xs-2";
+                thEventLocations.Text = "Locations";
+				thEventLocations.CssClass = "col-xs-6";
+                tableHeaderRow.Cells.Add( thName );
+                tableHeaderRow.Cells.Add( thEventStartEnd );
+                tableHeaderRow.Cells.Add( thEventLocations );
+                table.Rows.Add( tableHeaderRow );
+
+                foreach (ReservationService.ReservationSummary reservation in reservationSummaryList)
+                {
+
+                    if (reservation.ReservationStartDateTime.Date == day.Date)
+                    {
+
+
+                        TableRow trReservation = new TableRow();
+                        TableCell resName = new TableCell();
+                        TableCell resStartEnd = new TableCell();
+                        TableCell resEvent = new TableCell();
+
+
+                        Label reservationNameLabel = new Label();
+                        reservationNameLabel.Text = String.Format( "{0}", reservation.ReservationName );
+                        resName.Controls.Add( reservationNameLabel );
+                        trReservation.Cells.Add( resName );
+
+                        Label reservationStartEndLabel = new Label();
+                        reservationStartEndLabel.Text = String.Format( "{0} - {1}", reservation.ReservationStartDateTime.ToString("hh:mm tt"), reservation.ReservationEndDateTime.ToString( "hh:mm tt" ) );
+                        resStartEnd.Controls.Add( reservationStartEndLabel );
+                        trReservation.Cells.Add( resStartEnd );
+
+                        foreach (ReservationLocation rl in reservation.ReservationLocations)
+                        {
+                            Label reservationEventLabel = new Label();
+                            reservationEventLabel.Text = String.Format( "{0}, ", rl.Location.Name );
+                            resEvent.Controls.Add( reservationEventLabel );
+                        }
+
+                        
+                        trReservation.Cells.Add( resEvent );
+
+
+
+                        table.Rows.Add( trReservation );
+                    }
+
+                }
+                resDayPanel.Controls.Add( table );
+                resDayPanel.CssClass = "col-xs-12";
+                mainDiv.Controls.Add( resDayPanel );
+            }
+
+        }
+        
+    }
+
+    public IEnumerable<DateTime> EachDay( DateTime from, DateTime thru )
+    {
+        for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays( 1 ))
+            yield return day;
+    }
+}
